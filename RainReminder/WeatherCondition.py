495489e3519a6b3e248cd87184e8c7be570aee46
Biagio Piraino <biagiopirainowork@gmail.com>
2023-02-28 17:37:52 +0000

    introduced enums to define weather conditions

--- RainReminder/WeatherCondition.py
+++ RainReminder/WeatherCondition.py
@@ -1,13 +1,16 @@
 from datetime import datetime
+from WeatherEnums import WeatherCondition
+from WeatherEnumParser import WheatherEnumParser
 
 class WeatherCondition:
   def __init__(self, 
                 forecast_time: datetime, 
                 wheather_id: int,
                 description: str ) -> None:
-      self.__forecast_time = forecast_time
-      self.__whether_id    = wheather_id
-      self.__description   = description
+      self.__forecast_time      = forecast_time
+      self.__whether_id         = wheather_id
+      self.__description        = description
+      self.__weather_condition  = self.__parseWeatherId()
 
   def GetForecastTime(self) -> datetime:
     return self.__forecast_time
@@ -17,3 +20,9 @@ class WeatherCondition:
   
   def GetDescription(self) -> str:
     return self.__description
+  
+  def GetWeatherCondition(self) -> WeatherCondition:
+    return self.__weather_condition
+  
+  def __parseWeatherId(self) -> WeatherCondition:
+    return WheatherEnumParser.ParseWeatherId(self.__whether_id)
---
+++ RainReminder/WeatherEnums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class WeatherCondition(Enum):
+  Unknown      = 0
+  Thunderstorm = 1
+  Drizzle      = 2
+  Rain         = 3
+  Snow         = 4
+  Clear        = 5
+  Clouds       = 6
+
+  
