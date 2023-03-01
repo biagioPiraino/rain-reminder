6f34d03e855910787f72c847b86282610498672e
Biagio Piraino <biagiopirainowork@gmail.com>
2023-03-01 20:57:03 +0000

    actual logic of message sender implemented

--- RainReminder/MessageSender.py
+++ RainReminder/MessageSender.py
@@ -1,5 +1,7 @@
+import smtplib as smt
 from WeatherCondition import WeatherCondition
 from MessageOriginator import MessageOriginator as Originator
+from ParametersReader import ParametersReader as PR
 
 class MessageSender:
   def __init__(self) -> None:
@@ -7,4 +9,20 @@ class MessageSender:
 
   @classmethod
   def SendMessage(self, weather_condition: WeatherCondition) -> None:
-    print(Originator.OriginateMessage(weather_condition))
+    try:
+      with self.__connect() as connection:
+        connection.sendmail(
+          from_addr=connection.user, 
+          to_addrs=PR.RetrieveRecipientEmail(), 
+          msg=Originator.OriginateMessage(weather_condition))
+    except (smt.SMTPHeloError, smt.SMTPAuthenticationError,
+      smt.SMTPConnectError, smt.SMTPException) as error:
+      print(error)
+
+  @classmethod
+  def __connect(self) -> smt.SMTP:
+    smtp_connection = smt.SMTP(PR.RetrieveConnectionString())
+    smtp_connection.starttls()
+    credentials = PR.RetrieveSenderCredentials() 
+    smtp_connection.login(user=credentials["email"], password=credentials["password"])
+    return smtp_connection
