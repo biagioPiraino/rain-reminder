a0d0ddc92e250557c60962008349c24d27eaec96
Biagio Piraino <biagiopirainowork@gmail.com>
2023-03-01 16:27:20 +0000

    parameter reader logic updated to allow requester to get all paramters

--- RainReminder/ParametersReader.py
+++ RainReminder/ParametersReader.py
@@ -1,4 +1,6 @@
 import json
+import os
+from pathlib import Path
 
 class ParametersReader:
   def __init__(self) -> None:
@@ -6,28 +8,39 @@ class ParametersReader:
 
   @classmethod
   def RetrieveApiEndpoint(self) -> str:
-    with open(file="parameters.json", mode="r") as file:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(file=Path(current_directory, "parameters.json"), mode="r") as file:
       data = json.load(file)
       return data["api_endpoint"]
   
   @classmethod
   def RetrieveApiKey(self) -> str:
-    with open(file="parameters.json", mode="r") as file:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(file=Path(current_directory, "parameters.json"), mode="r") as file:
       data = json.load(file)
       return data["api_key"]
   
   @classmethod
   def RetrieveHoursRange(self) -> int:
-    with open(file="parameters.json", mode="r") as file:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(file=Path(current_directory, "parameters.json"), mode="r") as file:
       data = json.load(file)
       return int(data["hours_range"])
   
   @classmethod
   def RetrieveCurrentPosition(self) -> dict:
-    with open(file="parameters.json", mode="r") as file:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(file=Path(current_directory, "parameters.json"), mode="r") as file:
       data = json.load(file)
       current_position = {
         "lat" : data["current_position"]["latitude"],
         "lon" : data["current_position"]["longitude"]
       }
       return current_position
+  
+  @classmethod
+  def RetrieveExcludeData(self) -> str:
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    with open(file=Path(current_directory, "parameters.json"), mode="r") as file:
+      data = json.load(file)
+      return data["exclude_data"]
